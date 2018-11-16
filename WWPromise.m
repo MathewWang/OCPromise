61d636bbc2d5ab4f38fdf7520a534cf3a425f7e9
MathewWang <wangwei_2000@126.com>
2018-11-16 14:28:11 +0800

    增加同步支持

--- WWPromise.m
+++ WWPromise.m
@@ -3,10 +3,16 @@
 //  ORMiddleWare
 //
 //  Created by mathewwang on 2018/10/23.
-//  Copyright © 2018年 oriente. All rights reserved.
+//  Copyright © 2018年 mathewwang. All rights reserved.
 //
 
 #import "WWPromise.h"
+
+typedef NS_ENUM(NSInteger, ORPromiseState){
+    ORPromiseStateProcessing = 0,
+    ORPromiseStateResolved,
+    ORPromiseStateRejected
+};
 
 @interface WWPromise()
 
@@ -14,6 +20,10 @@
 @property (nonatomic, copy) RejectFunc rejectFunc;
 @property (nonatomic, strong) WWPromise *next;
 @property (nonatomic, copy) void(^executionFunc)(ResolveFunc, RejectFunc);
+@property (nonatomic, assign) ORPromiseState state;
+@property (nonatomic, strong) id result;
+@property (nonatomic, strong) NSError *error;
+
 @end
 
 @implementation WWPromise
@@ -29,43 +39,75 @@
     if (self = [super init]) {
         self.executionFunc = func;
         id(^tmpResolve)(id) = ^(id result){
+            self.result = result;
+            self.state = ORPromiseStateResolved;
             if (self.resolveFunc) {
                 id preReturnValue = self.resolveFunc(result);
-                if ([preReturnValue isKindOfClass:[WWPromise class]]) {
-                    [(WWPromise *)preReturnValue setResolveFunc:self.next.resolveFunc];
-                    [(WWPromise*)preReturnValue setRejectFunc:self.next.rejectFunc];
-                    self.next = preReturnValue;
-                    self.next.then(self.next.resolveFunc, self.next.rejectFunc);
-                }else{
-                    if (self.next && (self.next.resolveFunc || self.rejectFunc)) {
-                        self.executionFunc(self.next.resolveFunc, self.next.rejectFunc);
-                    }
-                }
+                //根据resolve的结果进行决议
+                [self p_ajust:preReturnValue];
                 return preReturnValue;
             }
-            return [WWPromise empty];
+            return [ORPromise empty];
         };
         void(^tmpError)(id) = ^(id error){
+            self.error = error;
+            self.state = ORPromiseStateRejected;
             if (self.rejectFunc) {
                 self.rejectFunc(error);
             }
         };
+
+        self.state = ORPromiseStateProcessing;
         self.executionFunc(tmpResolve, tmpError);
     }
     return self;
 }
 
-- (WWPromise *(^)(ResolveFunc, RejectFunc))then{
+- (void)p_ajust:(id) preRes{
+    if ([preRes isKindOfClass:[ORPromise class]]) {
+        [(ORPromise *)preRes setResolveFunc:self.next.resolveFunc];
+        [(ORPromise*)preRes setRejectFunc:self.next.rejectFunc];
+        self.next = preRes;
+        self.next.then(self.next.resolveFunc, self.next.rejectFunc);
+    }else{
+        if (self.next) {
+            if (self.state == ORPromiseStateResolved) {
+                self.next.state = ORPromiseStateResolved;
+                self.next.result = self.result;
+                if (self.next.resolveFunc) {
+                    [self.next p_ajust:self.next.resolveFunc(self.result)];
+                }
+            }else if (self.state == ORPromiseStateRejected){
+                self.next.state = ORPromiseStateRejected;
+                self.next.error = self.error;
+                self.next.rejectFunc(self.error);
+            }
+        }
+    }
+}
+
+- (ORPromise *(^)(ResolveFunc, RejectFunc))then{
     return ^(ResolveFunc resolve, RejectFunc reject){
         self.resolveFunc = resolve;
         self.rejectFunc = reject;
-        self.next = [WWPromise new];
+        
+        if (self.state == ORPromiseStateResolved) {
+            id preReturnValue = self.resolveFunc(self.result);
+            if ([preReturnValue isKindOfClass:[ORPromise class]]) {
+                self.next = preReturnValue;
+            }else{
+                return self;
+            }
+        }
+        
+        if (self.state == ORPromiseStateRejected){
+            self.rejectFunc(self.error);
+        }
+        if (!self.next) {
+            self.next = [ORPromise new];
+        }
         return self.next;
     };
-}
-
-+ (id)empty{
-    return nil;
 }
 
 - (void)dealloc{
